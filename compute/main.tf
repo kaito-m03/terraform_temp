9b4deb4f82003edf3b8283b17252740a206099ee
kaito-m03 <140139268+kaito-m03@users.noreply.github.com>
2024-01-09 14:52:12 +0000

    updated main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -12,3 +12,19 @@ provider "google" {
   region  = "asia-northeast1"
   zone    = "asia-northeast1-b"
 }
+
+resource "google_compute_instance" "<set_name>" {
+  name         = "<set_name>"
+  machine_type = "<machine-type>"
+  tags         = ["http-server","https-server"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      }
+    }
+  }
